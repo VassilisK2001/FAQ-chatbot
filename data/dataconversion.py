04e94a1c0d10d971e1ddb6105a26e02be7801858
Βασίλης <vassilios.kekessis@gmail.com>
2024-10-24 19:29:08 +0300

    developed script for converting dataset to JSONL format

--- data/dataconversion.py
+++ data/dataconversion.py
@@ -1,12 +1,38 @@
 import json
 
-input_file_path = "./data/shuffled_faq_dataset.json"
+# Define input json file and target jsonl file
+input_file_path = "./data/faq_dataset.json"
 output_file_path = "./data/faq_dataset_converted.jsonl"
 
 def convert_json_to_chat(input_file, output_file):
-    # Load the questions and answers from the input JSON file
-    with open(input_file, 'r') as file:
-        data = json.load(file)
+
+    try:
+        # Load the questions and answers from the input JSON file
+        with open(input_file, 'r') as file:
+            data = json.load(file)
+        
+        with open(output_file, 'w') as file:
+            # Iterate over each question and answer pair and write the chat format to the output file
+            for item in data:
+                chat_format = {
+                    "messages": [
+                        {"role": "user", "content": item.get("question")},
+                        {"role": "assistant", "content": item.get("answer")}
+                    ]
+                }
+
+                # Write the chat conversation as a json line in the output file
+                file.write(json.dumps(chat_format) + '\n')
+
+        print(f"The dataset from {input_file} was successfully converted to {output_file}")
+    except Exception as e:
+        
+        print(f"An error occured: {e}")
+        
+
+# Call the function to perform the conversion
+convert_json_to_chat(input_file_path, output_file_path)
+
     
    
 
