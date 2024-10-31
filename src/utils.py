b533eb8116032a9da5052fca3ff290a8e6b5da45
Βασίλης <vassilios.kekessis@gmail.com>
2024-10-31 20:57:49 +0200

    modified utils.py

--- src/utils.py
+++ src/utils.py
@@ -5,13 +5,22 @@ import dotenv
 dotenv.load_dotenv()
 
 # Helper function for creating finetuning job
-def create_fine_tuning(model_name, openai_key, train_id, eval_id):
+def create_fine_tuning(model_name, openai_key, train_id, eval_id, hyperparameters=None):
     client = OpenAI(api_key = openai_key)
 
-    fine_tuning_response = client.fine_tuning.jobs.create(
-        model = model_name,
-        training_file = train_id,
-        validation_file = eval_id
-    )
+    args = {
+        "model": model_name,
+        "training_file": train_id,
+        "validation_file": eval_id
+    }
+
+    # Check if hyperparameters argument provided and ansure its a dictionary
+    if (hyperparameters is not None):
+        if (isinstance(hyperparameters, dict)):
+            args["hyperparameters"] = hyperparameters
+        else:
+            raise TypeError("hyperparameters must be a dictionary if provided")
+
+    fine_tuning_response = client.fine_tuning.jobs.create(**args)
 
     return fine_tuning_response
